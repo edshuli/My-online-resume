a17cf0cacbe23d4d5f3f4fff16dbb69aa1c80c06
Ubuntu <ubuntu@ip-172-31-34-70.us-east-2.compute.internal>
2019-10-07 15:01:45 +0000

    Implement emails integration on contact page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,7 +37,7 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-    
+
     $("#gh-user-data").html("");
     $("#gh-repo-data").html("");
 
@@ -66,6 +66,10 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            }
+            else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             }
             else {
                 console.log(errorResponse);
